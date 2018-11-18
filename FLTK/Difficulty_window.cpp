af032a85b338cf4850233f90b422b381817c1394
leuela48 <leuela48@tamu.edu>
2018-11-17 19:34:03 -0600

    Task 2 FLTK

---
+++ FLTK/Difficulty_window.cpp
@@ -0,0 +1,65 @@
+//Difficulty_window.cpp
+
+#include "GUI.h"
+#include "Graph.h"
+#include "FL/Fl_JPEG_Image.H"
+#include "std_lib_facilities_5.h"
+
+using namespace Graph_lib;
+
+struct Difficulty_window : Graph_lib::Window {
+    
+    Difficulty_window(Point xy, int w, int h, const string& title) : // User chooses game difficulty in this window
+        Window(xy,w,h,title),
+		input_n(Point(400,100),50,20,"Please enter number of pancakes (2-9): "),
+		input_d(Point(900,100),50,20,"Please enter difficulty (1-n): "),
+		submit_button(Point(int(x_max()/2)-200,y_max()-200), 400, 100, "Submit", cb_submit),
+        running(true)
+    {
+        attach(input_n);
+        attach(input_d);
+        attach(submit_button);
+    }
+    void wait_for_button(int &numOfPancakes, int &difficulty)
+    {
+        while(running) Fl::wait(0);
+        hide();
+        numOfPancakes = n;
+		difficulty = d;
+    }
+    
+private:
+    int n, d;
+	In_box input_n;
+	In_box input_d;
+	Button submit_button;
+    bool running;
+	
+	
+	static void cb_submit(Address, Address pw){reference_to<Difficulty_window>(pw).submit();}
+    void submit(){
+		n = input_n.get_int();
+		d = input_d.get_int();
+		if(n < 2 || n > 9 || d < 1 || d > n) {
+			//Text invalid_input(Point(500,200),"INVALID INPUT!");
+			//attach(invalid_input);
+			cout << "Invalid input!" << endl;
+			running = true;
+		}
+		else	
+			running = false;
+	}
+	
+    // static void cb_easy(Address, Address pw){reference_to<Difficulty_window>(pw).easy();}
+    // void easy(){difficulty = string("easy"); running = false;}
+
+    // static void cb_medium(Address, Address pw){reference_to<Difficulty_window>(pw).medium();}
+    // void medium(){difficulty = string("medium"); running = false;}
+
+    // static void cb_hard(Address, Address pw){reference_to<Difficulty_window>(pw).hard();}
+    // void hard(){difficulty = string("hard"); running = false;}
+
+    // static void cb_extreme(Address, Address pw){reference_to<Difficulty_window>(pw).extreme();}
+    // void extreme(){difficulty = string("extreme"); running = false;}
+
+};
---
+++ FLTK/TestMain.cpp
@@ -0,0 +1,67 @@
+//#include "Start_window.h"
+#include "Difficulty_window.cpp"
+#include <FL/Fl_Text_Display.H>
+//#include "Game_window.cpp"
+//#include "InputName_window.cpp"
+//#include "GameOver_window.cpp"
+
+// vector<string> getScores(string &difficulty);
+// void writeScores(vector<string>&, string);
+int numOfPancakes, difficulty;
+void start(bool&);
+
+int main()
+{
+    const int WINDOW_WIDTH = 1280;
+    const int WINDOW_HEIGHT = 720;
+   
+    //Start splash window
+    //Start_window window(Point(200, 200), WINDOW_WIDTH, WINDOW_HEIGHT, "Pancake!");
+    //window.wait_for_button();
+    
+	bool run = true;
+	start(run);
+	
+	// bool playagain = false;
+	// do{
+		// start(playagain);
+	// }while(playagain);
+
+    // return 0;
+}
+void start(bool &playagain)
+{
+	const int WINDOW_WIDTH = 1280;
+    const int WINDOW_HEIGHT = 720;
+	
+	// select difficulty
+	Difficulty_window diff_window(Point(200, 200), WINDOW_WIDTH, WINDOW_HEIGHT, "Pancake!");
+    diff_window.wait_for_button(numOfPancakes, difficulty);
+
+	// //Select difficulty window and store in string difficulty
+    // string difficulty = "";
+    // Difficulty_window diff_window(Point(200, 200), WINDOW_WIDTH, WINDOW_HEIGHT, "POOZLE!");
+    // diff_window.wait_for_button(difficulty);
+    // cout << ":: " << difficulty << endl;
+
+	// //Receive name window and store in player_name
+    // string player_name = "Anonymous";
+	// InputName_window name_window(Point(200,200), WINDOW_WIDTH, WINDOW_HEIGHT, "POOZLE!");
+	// name_window.wait_for_button(player_name);
+	// cout << ":: " << player_name << endl;
+
+	// //Prepare input file for game window
+	// vector<string> scores = getScores(difficulty); 
+	// //Game window
+	// Game_window game_window(Point(200,200), WINDOW_WIDTH, WINDOW_HEIGHT, "POOZLE!", difficulty, scores, player_name);
+    // game_window.wait_for_button();
+
+	// //Game over window
+	// GameOver_window gameover_window(Point(200,200), WINDOW_WIDTH, WINDOW_HEIGHT, "POOZLE!", scores, playagain);
+	// gameover_window.wait_for_button();
+
+	//writeScores(scores, difficulty);
+	
+	cout << "numOfPancakes = " << numOfPancakes << endl;
+	cout << "difficulty = " << difficulty << endl;
+}
