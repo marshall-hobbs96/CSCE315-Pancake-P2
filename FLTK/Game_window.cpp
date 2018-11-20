f316c0de783d23d300c06e2597c961f1f294d8d8
Marshall <marshall.hobbs96@tamu.edu>
2018-11-19 21:20:25 -0600

    things

--- FLTK/Game_window.cpp
+++ FLTK/Game_window.cpp
@@ -13,7 +13,7 @@ struct Game_window : Graph_lib::Window {
     
     Game_window(Point xy, int w, int h, const string& title, Game* game) :
         Window(xy,w,h,title),
-		submit_button(Point(int(x_max()/2)-200,y_max()-200), 400, 100, "Submit", cb_submit),
+		submit_button(Point(530,y_max()-55), 200, 50, "Submit", cb_submit),
         curr_game(game),
         running(true)
     {
@@ -21,16 +21,27 @@ struct Game_window : Graph_lib::Window {
     }
     bool wait_for_button()
     {
-        Text title(Point(450,75),"Game Window!");
+        Text title(Point(500,75),"Game Window!");
         title.set_font(Graph_lib::Font::helvetica_bold);
         title.set_font_size(30);
         attach(title);
 
-        Ellipse** ellsHuman = new Ellipse*[9];
-        Ellipse** ellsAI = new Ellipse*[9];
+        Text player_title(Point(300, 100), "Player Stack");
+        Text ai_title(Point(800, 100), "AI Stack");
+        player_title.set_font(Graph_lib::Font::helvetica_bold);
+        player_title.set_font_size(20);
+        ai_title.set_font(Graph_lib::Font::helvetica_bold);
+        ai_title.set_font_size(20);
+        attach(player_title);
+        attach(ai_title);
 
-        drawPancakes(curr_game->getHumanStack(), ellsHuman, curr_game->getStackSize(), true, -1);
-        drawPancakes(curr_game->getAIStack(), ellsAI, curr_game->getStackSize(), false, -1);
+        Ellipse** ellsHuman = new Ellipse*[9];
+        Text** humanNums = new Text*[9];
+        Ellipse** ellsAI = new Ellipse*[9];
+        Text** AINums = new Text*[9];
+
+        drawPancakes(curr_game->getHumanStack(), ellsHuman, humanNums, curr_game->getStackSize(), true, -1);
+        drawPancakes(curr_game->getAIStack(), ellsAI, AINums, curr_game->getStackSize(), false, -1);
         int AI_selection = curr_game->getAIMove();
         int AIFlip = curr_game->getStackSize() - AI_selection;
 
@@ -39,10 +50,13 @@ struct Game_window : Graph_lib::Window {
         for(int j = 0; j < curr_game->getStackSize(); j++){
 
             detach(*ellsAI[j]);
+            detach(*AINums[j]);
             delete ellsAI[j];
+            delete AINums[j];
 
         }
-        drawPancakes(curr_game->getAIStack(), ellsAI, curr_game->getStackSize(), false, AIFlip);
+
+        drawPancakes(curr_game->getAIStack(), ellsAI, AINums, curr_game->getStackSize(), false, AIFlip);
         curr_game->moveAI(AI_selection);
 
 
@@ -50,16 +64,23 @@ struct Game_window : Graph_lib::Window {
         for(int j = 0; j < curr_game->getStackSize(); j++){
 
             detach(*ellsAI[j]);
+            detach(*AINums[j]);
             delete ellsAI[j];
+            delete AINums[j];
 
         }
 
-        drawPancakes(curr_game->getAIStack(), ellsAI, curr_game->getStackSize(), false, -1);
+        drawPancakes(curr_game->getAIStack(), ellsAI, AINums, curr_game->getStackSize(), false, -1);
 
         while(running) Fl::wait(0);
 
         delete ellsHuman;
         delete ellsAI;
+        delete humanNums;
+        delete AINums;
+
+        //add return win condition from curr_game++++++++++++++++++++++++++
+
         hide();
     }
     
@@ -75,21 +96,40 @@ private:
 		running = false;
 	}
 
-    void drawPancakes(int* stack, Ellipse** ells, int stack_size, bool player, int blink_from){
+    static void cb_wait(void* ){
+        
+        sleep(3);
+        return;
+    }
+
+    void drawPancakes(int* input_stack, Ellipse** ells, Text** text, int stack_size, bool player, int blink_from){
+
+        int* stack = new int[9];
+
+        for(int j = 0; j < stack_size; j++){
+
+            stack[j] = input_stack[stack_size - 1 - j];
+
+        }
 
         if(player == true){             //drawing player stack
             for(int i = 0; i < stack_size; i++) {
-
-                ells[i] = new Ellipse(Point(100, 200 + 20 * i), 15 * stack[i], 10);
+                
+                ells[i] = new Ellipse(Point(360, 160 + 56 * i), 17 * stack[i], 27);
                 ells[i]->set_fill_color(Color::red);
+                string str = to_string(stack[i]);
+                text[i] = new Text(Point(356, 165 + 56 * i), str);
+                text[i]->set_font(Graph_lib::Font::helvetica_bold);
+                text[i]->set_font_size(15);
 
                 if(i >= blink_from) {           //the point from which the player chose to flip from
 
-                    ells[i]->set_fill_color(Color::blue);
+                    ells[i]->set_fill_color(Color::yellow);
 
                 }
 
                 attach(*ells[i]);
+                attach(*text[i]);
 
 
             }
@@ -100,16 +140,21 @@ private:
 
             for(int i = 0; i < stack_size; i++) {
 
-                ells[i] = new Ellipse(Point(500, 200 + 20 * i), 15 * stack[i], 10);
+                ells[i] = new Ellipse(Point(860, 160 + 56 * i), 17 * stack[i], 27);
                 ells[i]->set_fill_color(Color::red);
+                string str = to_string(stack[i]);
+                text[i] = new Text(Point(856, 165 + 56 * i), str);
+                text[i]->set_font(Graph_lib::Font::helvetica_bold);
+                text[i]->set_font_size(15);
 
                 if(i >= blink_from) {           //the point from which the player chose to flip from
 
-                    ells[i]->set_fill_color(Color::blue);
+                    ells[i]->set_fill_color(Color::yellow);
 
                 }
 
                 attach(*ells[i]);
+                attach(*text[i]);
 
 
             }
--- FLTK/TestMain.cpp
+++ FLTK/TestMain.cpp
@@ -56,7 +56,7 @@ void start(bool &playagain)
 	bool game_won = false;
 
 	while(game_won == false){
-		Game_window game_window(Point(200, 200), WINDOW_WIDTH, WINDOW_HEIGHT, "Pancake!", currGame);
+		Game_window game_window(Point(50, 50), WINDOW_WIDTH, WINDOW_HEIGHT, "Pancake!", currGame);
 		game_won = game_window.wait_for_button();
 	}
 	//initials and score windows
