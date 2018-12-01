338f4e2cba3f773a910ed735705af32e4a3af41f
Samantha <samantharhay@tamu.edu>
2018-12-01 15:19:50 -0600

    working tkinter cpp

--- Tkinter/main.cpp
+++ Tkinter/main.cpp
@@ -1,61 +1,58 @@
 #include <iostream>
 #include <python2.7/Python.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <string>
 
 using namespace std;
 
 int main() {
-	// Set PYTHONPATH TO working directory
-   setenv("PYTHONPATH",".",1);
+	// // Set PYTHONPATH TO working directory
+    setenv("PYTHONPATH",".",1);
 
-   PyObject *pName, *pModule, *pDict, *pFunc, *pValue, *pArgs, *presult;
-
-
-   // Initialize the Python Interpreter
-   Py_Initialize();
+    PyObject *pName, *pModule, *pDict, *pFunc, *pValue, *presult;
 
 
-   // Build the name object
-   pName = PyString_FromString((char*)"script");
-
-   // Load the module object
-   pModule = PyImport_Import(pName);
-
-   // pDict is a borrowed reference 
-   pDict = PyModule_GetDict(pModule);
+    // Initialize the Python Interpreter
+    Py_Initialize();
 
 
-   // pFunc is also a borrowed reference 
-   pFunc = PyDict_GetItemString(pDict, (char*)"printTest");
-   
-   // make python tuple
-   pArgs = PyTuple_New(2);
-   pValue = PyInt_FromLong(2);
-   
-   PyTuple_SetItem(pArgs, 0, pValue);
-   PyTuple_SetItem(pArgs, 1, pValue);
+    // Build the name object
+    pName = PyString_FromString((char*)"script");
 
-   if (PyCallable_Check(pFunc))
-   {
-	   pValue=Py_BuildValue("ss","some1","some2");
-       PyErr_Print();
-       printf("Let's give this a shot!\n");
-       presult=PyObject_CallObject(pFunc,pValue);
-       PyErr_Print();
-   } else 
-   {
-       PyErr_Print();
-   }
-   printf("Result is %d\n",PyInt_AsLong(presult));
-   Py_DECREF(pValue);
+    // Load the module object
+    pModule = PyImport_Import(pName);
 
-   // Clean up
-   Py_DECREF(pModule);
-   Py_DECREF(pName);
+    // pDict is a borrowed reference 
+    pDict = PyModule_GetDict(pModule);
 
-   // Finish the Python Interpreter
-   Py_Finalize();
 
+    // pFunc is also a borrowed reference 
+    pFunc = PyDict_GetItemString(pDict, (char*)"printTest");
+	
+    if (PyCallable_Check(pFunc))
+    {
+	    int value = 123;
+        pValue=Py_BuildValue("i",value);
+		PyErr_Print();
+        printf("Let's give this a shot!\n");
+        presult=PyObject_CallObject(pFunc,NULL);
+        PyErr_Print();
+    } else 
+    {
+       // PyErr_Print();
+    }
+    printf("Result is %lu\n",PyInt_AsLong(presult));
+    Py_DECREF(pValue);
+
+    // Clean up
+    Py_DECREF(pModule);
+    Py_DECREF(pName);
+
+    // Finish the Python Interpreter
+    Py_Finalize();
+
+	
 
    return 0;	
 }
--- Tkinter/script.py
+++ Tkinter/script.py
@@ -1,9 +1,12 @@
-# def main():
-	# printTest('a')
-	
-def printTest(a, b):
-	print("Python script was called")
-	print(a)
-	print(b)
+from Tkinter import *
+def printTest():
+	sys.argv = ["Main"]
+	window = Tk()
+	window.title("Discrepency")
+	wind_width = 1200
+	wind_height = 800
+	c = Canvas(window, width = wind_width, height = wind_height)
+	c.pack()
+	window.mainloop()
 	return 5
-# main()
+	
