7518678ee5104a976de8cf44bc51ae4aab9a3ed9
leuela48 <leuela48@tamu.edu>
2018-12-01 16:20:29 -0600

    Complete with Dynamic Sliders

--- Tkinter/Task2.py
+++ Tkinter/Task2.py
@@ -3,31 +3,20 @@ Leuel Asnake
 """
 
 from Tkinter import *
-
-numOfPancakes = -1
-
-def setP(p):
-	return p
-	#numOfPancakes = p
-
-def drawPancakeButtons(w):
-	pb2 = Button(w, text="2", command=lambda:setP(2), height=2, width=5)
-	pb2.place(x=250,y=200)
-	pb3 = Button(w, text="3", command=lambda:setP(3), height=2, width=5)
-	pb3.place(x=350,y=200)
-	pb4 = Button(w, text="4", command=lambda:setP(4), height=2, width=5)
-	pb4.place(x=450,y=200)
-	pb5 = Button(w, text="5", command=lambda:setP(5), height=2, width=5)
-	pb5.place(x=550,y=200)
-	pb6 = Button(w, text="6", command=lambda:setP(6), height=2, width=5)
-	pb6.place(x=650,y=200)
-	pb7 = Button(w, text="7", command=lambda:setP(7), height=2, width=5)
-	pb7.place(x=750,y=200)
-	pb8 = Button(w, text="8", command=lambda:setP(8), height=2, width=5)
-	pb8.place(x=850,y=200)
-	pb9 = Button(w, text="9", command=lambda:setP(9), height=2, width=5)
-	pb9.place(x=950,y=200)
 	
+def choosePancakes (w):
+	w.create_text(500,160,text="Select Number of Pancakes:",font=("Arial",20), anchor = NW)
+	
+	pancakeScale = Scale(w, from_ = 2, to = 9, tickinterval = 1, length = 1000, orient=HORIZONTAL, sliderlength = 30, font=("Arial",25))
+	pancakeScale.place(x=150,y=200)
+	
+	nextB = Button(w, text="Next", command=lambda:chooseDifficulty(w, pancakeScale.get()), height=3, width=10)
+	nextB.place(x=600,y=350)	
+	
+def chooseDifficulty(w, n):
+	w.create_text(500,460,text="Select Difficulty:",font=("Arial",20), anchor = NW)
+	diffScale = Scale(w, from_ = 1, to = n, tickinterval = 1, length = 1000, orient=HORIZONTAL, sliderlength = 30, font=("Arial",25))
+	diffScale.place(x=150,y=500)
 	
 def drawDifficultyWindow():
 	master = Tk()
@@ -35,14 +24,11 @@ def drawDifficultyWindow():
 	w.pack()
 
 	w.create_text(520,50,text="Set Your Difficulty!",font=("Arial",30), anchor = NW)
-	w.create_text(500,125,text="Select Number of Pancakes:",font=("Arial",20), anchor = NW)
-	#drawPancakeButtons(w)
 	
-	pancakeScale = Scale(w, from_ = 2, to = 9, tickinterval = 1, length = 1000, orient=HORIZONTAL, sliderlength = 30, font=("Arial",25), command=setP)
-	print ( pancakeScale.place(x=150,y=200) )
-
-
+	choosePancakes(w)
 	
+	submitB = Button(w, text="Submit", height=3, width=10)
+	submitB.place(x=600,y=650)
 	
 	w.mainloop()
 
