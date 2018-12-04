7c6ecee3f2ebe68dafb0ad714013cc8ce0b22e5e
marshall-hobbs96 <marshall.hobbs96@tamu.edu>
2018-12-03 23:15:13 -0600

    Add files via upload

---
+++ Tkinter/tie_window.py
@@ -0,0 +1,20 @@
+from Tkinter import *
+from time import *
+
+def tie_window:
+    for widget in window.winfo_children():
+        widget.destroy()
+
+    win_label = Label(window, text = "YOU TIED!!!!!", font = ("Comic Sans MS", 50), fg = "red")
+    win_label.place(relx= 0.5, rely = 0.5, anchor = CENTER)
+    for x in range(50):
+        window.configure(background = "green")
+        win_label.configure(fg="red", bg = "green")
+        window.update()
+        sleep(0.1)
+        window.update()
+        window.configure(background = "red")
+        win_label.configure(fg = "green", bg = "red")
+        window.update()
+        sleep(0.1)
+        window.update()
