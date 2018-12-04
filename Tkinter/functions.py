6761a61a89255d4b7bb3d55fb21be9f04d49ec3c
hannahhut <hannahhut@tamu.edu>
2018-12-03 20:55:45 -0600

    Added score functions to function.py

--- Tkinter/functions.py
+++ Tkinter/functions.py
@@ -444,3 +444,124 @@ def ai_turn(player_stacks, ai_stacks, ai_moves, window):
     return_stack = return_stack[::-1]
     return return_stack
 
+def initials(window, status):	#status is a string with a message about the winner
+	initials = ""
+	
+	#give comment depending on winner
+	if status == 0:
+		comment = "Sorry, the AI won!"
+	elif status == 1:
+		comment = "Congratulations, you won!"
+	else:
+		comment = "It was a tie!"
+		
+	#fillers are used to fix spacing in the grid
+	fill = Label(window, text = "         ", font=("Arial Bold", 120))
+	fill.pack()
+	
+	lbl = Label(window, text=comment, font=("Arial Bold", 40))
+	lbl.pack()
+	
+	lbl2 = Label(window, text="Please Enter Your Initials Below", font=("Arial Bold", 35))
+	lbl2.pack()
+	
+	fill2 = Label(window, text = "            ", font=("Arial Bold", 60))
+	fill2.pack()
+
+	txt = Entry(window,width=20)
+	txt.pack()
+	
+	def clicked():
+		initials = txt.get()
+		fill.destroy()
+		lbl.destroy()
+		lbl2.destroy()
+		fill2.destroy()
+		fill3.destroy()
+		btn.destroy()
+		txt.destroy()
+		window.quit()
+
+	fill3 = Label(window, text = "            ", font=("Arial Bold", 25))
+	fill3.pack()	
+		
+	btn = Button(window, text="Enter", command=clicked)
+	btn.pack()
+	btn.config(height=2,width=20)
+
+	window.mainloop()
+	
+	return initials
+	
+def scores(window, scoreString):		#scores is a string holding the initials and high scores
+	cont = 0
+		
+	fill = Label(window, text = "                ", font=("Arial Bold", 60))
+	fill.pack()
+	
+	lbl = Label(window, text="High Scores", font=("Arial Bold", 40))
+	lbl.pack()
+	
+	spaces = 0
+	scoreCount = 0
+	score = ""
+	scores = []
+	for x in scoreString:
+		if x == " ":
+			if spaces == 0:
+				score = score + x
+				spaces = 1
+			else:
+				scores.append(score)
+				score = ""
+				spaces = 0
+		else: 
+			score = score + x
+	scores.append(score)
+	
+	#print the high scores
+	score1 = Label(window, text=scores[0], font=("Arial Bold", 35))
+	score1.pack()
+	score2 = Label(window, text=scores[1], font=("Arial Bold", 35))
+	score2.pack()
+	score3 = Label(window, text=scores[2], font=("Arial Bold", 35))
+	score3.pack()
+	score4 = Label(window, text=scores[3], font=("Arial Bold", 35))
+	score4.pack()
+	score5 = Label(window, text=scores[4], font=("Arial Bold", 35))
+	score5.pack()
+		
+	#fill2 = Label(window, text = " ", font=("Arial Bold", 30))
+	#fill2.grid(column=0,row=7)
+		
+	def replay():
+		cont = 1
+		fill.destroy()
+		lbl.destroy()
+		replay.destroy()
+		fill2.destroy()
+		end.destroy()
+		score1.destroy()
+		score2.destroy()
+		score3.destroy()
+		score4.destroy()
+		score5.destroy()
+		window.quit()
+
+	replay = Button(window, text="Play Again", command=replay)
+	replay.pack()
+	replay.config(height=2,width=20)
+	
+	fill2 = Label(window, text = " ", font=("Arial Bold", 25))
+	fill2.pack()
+	
+	def end():
+		window.destroy()
+
+	end = Button(window, text="End Game", command=end)
+	end.pack()
+	end.config(height=2,width=20)	
+		
+	window.mainloop()
+	
+	return cont # continue - if player wants to play again
