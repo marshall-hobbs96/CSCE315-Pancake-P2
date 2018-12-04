250b0038d8fb7a6b8950686c92f800bbaaff41c6
hannahhut <hannahhut@tamu.edu>
2018-12-03 21:36:13 -0600

    Added files for extra credit

---
+++ Tkinter/sample_main5.cpp
@@ -0,0 +1,41 @@
+// #include "std_lib_facilities_5.h"
+// #include "Simple_window.h"
+// #include "Graph.h"
+// #include "FL/Fl_JPEG_Image.H"
+// int main()
+// try {
+	// if(H112 != 201708L)error("Error: incorrect std_lib_facilities_5.h version ",
+			   // H112);
+	// Simple_window win1(Point(100,200),600,400,"two lines");
+	
+	// Line horizontal(Point(100,100),Point(200,100));  // make a horizontal line
+	// Line horizontal2(Point(400,400),Point(500,400));
+	// Line vertical(Point(150,50),Point(150,150));     // make a vertical line
+	
+	// vertical.set_style(Line_style(Line_style::solid, 1));
+	// win1.attach(horizontal);
+	// win1.attach(horizontal2);
+	// win1.attach(vertical);
+	
+	// vertical.set_color(Color::magenta);
+	// horizontal.set_color(Color::black);
+	// horizontal2.set_color(Color::black);
+	
+	// win1.drawtext("This is text", 300, 400); 
+	
+	
+	
+	
+	
+	
+	// win1.wait_for_button();
+	// return 0;
+// }
+// catch(exception& e) {
+	// cerr << "exception: " << e.what() << '\n';
+	// return 1;
+// }
+// catch (...) {
+	// cerr << "Some exception\n";
+	// return 2;
+// }
