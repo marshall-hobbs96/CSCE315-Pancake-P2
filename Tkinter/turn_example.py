b2d73cbc854b1e489f7f6f6a1ab06dab7746350d
marshall-hobbs96 <marshall.hobbs96@tamu.edu>
2018-12-04 02:47:34 -0600

    Add files via upload

--- Tkinter/turn_example.py
+++ Tkinter/turn_example.py
@@ -11,17 +11,17 @@ import sys
 def calc_AI_move(ai_stack):
 	n = len(ai_stack)	
 	ans = n
-	i = n-1
+	i = 0
 	
 	while (ai_stack[i] == str(ans)):
 		#print("checking: ", ans)
 		ans-=1
-		i-=1
+		i+=1
 	
-	#print(ai_stack[0])
+	#print(ai_stack[n-1])
 	#print(ans)
 	
-	if (ai_stack[0] == str(ans)): # if top
+	if (ai_stack[n-1] == str(ans)): # if top
 		#print("in if top")
 		return str(ans)
 	else:
@@ -30,6 +30,7 @@ def calc_AI_move(ai_stack):
 	
 	#print("pancake number: ", pnum)	
  	loc = ai_stack.index(str(pnum)) + 1
+ 	loc = n-(loc-1)
 	#print("location: ", loc)
 	return str(loc)
 	#return str(ai_stack.index(pnum))
@@ -39,7 +40,7 @@ def main():
 
     
     
-    print('test')
+    #print('test')
     arg = sys.argv[1]
     stack = str(arg)
     player_stack = stack
@@ -48,34 +49,32 @@ def main():
     stack_size = len(stack);
     window = Tk()
     window.geometry("1280x720")
-    print(in_order_stack[0:stack_size])
     result = 2 #0 = loss, 1 = win, 2 = tie
     game_done = 0
     status = -1
-    whatflip = calc_AI_move(ai_stack)
     while(game_done != 1):
+        whatflip = calc_AI_move(ai_stack)
         player_stack = player_turn(player_stack, ai_stack, window)
         ai_stack = ai_turn(player_stack, ai_stack, whatflip, window)
-
 
         if player_stack[::-1] == in_order_stack[0:stack_size] and ai_stack[::-1] == in_order_stack[0:stack_size]:
             game_done = 1
             tie_window(window)
             result = 2
             status = 2
-            print result
+            #print result
         elif player_stack[::-1] == in_order_stack[0:stack_size]:
             game_done = 1
             win_window(window)
             result = 3
             status = 1
-            print result
+            #print result
         elif ai_stack[::-1] == in_order_stack[0:stack_size]:
             game_done = 1
             lose_window(window)
             result = 1
             status = 0
-            print result
+            #print result
 
     score = calcScore(result, 4, 4)
     scrs = readScore()
