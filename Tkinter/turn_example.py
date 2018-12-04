1cfd1779f38728546d7da1e08c280f66f29fe76c
McLainJ <mclainj@tamu.edu>
2018-12-03 23:39:52 -0600

    Update turn_example.py
    halp

--- Tkinter/turn_example.py
+++ Tkinter/turn_example.py
@@ -1,24 +1,38 @@
-from tkinter import *
+from Tkinter import *
 from player_turn import *
 from ai_turn import *
 from win_window import *
 from random import *
 from lose_window import *
+import sys
 
-game_done = 0
-player_stack = "512346789"
-ai_stack = "512346789"
-window = Tk()
-window.geometry("1080x720")
-while(game_done != 1):
-    player_stack = player_turn(player_stack, ai_stack, window)
-    ai_stack = ai_turn(player_stack, ai_stack, str(randint(1,9)) , window)
 
-    if((player_stack == "123456789")):
-        game_done = 1
-        win_window(window)
+def main():
+    print('test')
+    arg = sys.argv[1]
+    stack = str(arg)
+    player_stack = stack
+    ai_stack = stack
+    in_order_stack = "123456789"
+    stack_size = len(stack);
 
-    elif((ai_stack == "123456789")):
-        game_done = 1
-        lose_window(window)
+    window = Tk()
+    window.geometry("1080x720")
+
+    game_done = 0
+    while(game_done != 1):
+        player_stack = player_turn(player_stack, ai_stack, window)
+        ai_stack = ai_turn(player_stack, ai_stack, str(randint(1,9)) , window)
+
+
+        if player_stack == in_order_stack[0:stack_size] and ai_stack == in_order_stack[0:stack_size]:
+            game_done = 1
+            #tie_window(window)
+        elif player_stack == in_order_stack[0:stack_size]:
+            game_done = 1
+            win_window(window)
+        elif ai_stack == in_order_stack[0:stack_size]:
+            game_done = 1
+            lose_window(window)
+            
     
