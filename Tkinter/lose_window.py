675d58d6624f58a7f5a09f14ad81243701ae783c
marshall-hobbs96 <marshall.hobbs96@tamu.edu>
2018-12-04 01:25:23 -0600

    Add files via upload

--- Tkinter/lose_window.py
+++ Tkinter/lose_window.py
@@ -9,16 +9,18 @@ def lose_window(window):
 
     win_label = Label(window, text = "YOU LOSE!!!!\n YOU SUCK!!!!!", font = ("Comic Sans MS", 50), fg = "red")
     win_label.place(relx= 0.5, rely = 0.5, anchor = CENTER)
-    for x in range(30):
+    for x in range(15):
         window.configure(background = "green")
         win_label.configure(fg="red", bg = "green")
         window.update()
-        sleep(0.1)
+        sleep(0.2)
         window.update()
         window.configure(background = "red")
         win_label.configure(fg = "green", bg = "red")
         window.update()
-        sleep(0.1)
+        sleep(0.2)
         window.update()
 
     
+    for widget in window.winfo_children():
+        widget.destroy()
--- Tkinter/tie_window.py
+++ Tkinter/tie_window.py
@@ -7,15 +7,17 @@ def tie_window(window):
 
     win_label = Label(window, text = "YOU TIED!!!!!\n SPONGEBOB WOULD BE PROUD", font = ("Comic Sans MS", 50), fg = "red")
     win_label.place(relx= 0.5, rely = 0.5, anchor = CENTER)
-    for x in range(50):
+    for x in range(15):
         window.configure(background = "green")
         win_label.configure(fg="red", bg = "green")
         window.update()
-        sleep(0.1)
+        sleep(0.2)
         window.update()
         window.configure(background = "red")
         win_label.configure(fg = "green", bg = "red")
         window.update()
-        sleep(0.1)
+        sleep(0.2)
         window.update()
 
+    for widget in window.winfo_children():
+        widget.destroy()
